4cdd47117c633e01733cade3f749575a5366abea
Provi <zhangxian1942@live.com>
2020-08-08 01:01:35 -0700

    Sprite animation for moving

--- PlayerTank.gd
+++ PlayerTank.gd
@@ -7,16 +7,22 @@ onready var sprite = $Sprite
 var velocity = Vector2()
 
 func _physics_process(_delta):
-	#TODO: call sprite selection methods so that tank is animated
 	var input_vector = Vector2()
 	
 	if Input.is_action_pressed("ui_up"):
 		input_vector.y += -1
+		sprite.play()
 	elif Input.is_action_pressed("ui_down"):
 		input_vector.y += 1
+		sprite.play()
 	elif Input.is_action_pressed("ui_left"):
 		input_vector.x += -1
+		sprite.play()
 	elif Input.is_action_pressed("ui_right"):
 		input_vector.x += 1
+		sprite.play()
+	else:
+		sprite.stop()
 	velocity = input_vector * SPEED
+	sprite.set_dir_set(input_vector)
 	velocity = move_and_slide(velocity)
--- TankSprite.gd
+++ TankSprite.gd
@@ -1,3 +1,32 @@
 extends Sprite
 
-#TODO: methods for Sprite selection based on params
+# Sprite API
+
+var dir_set = [0, 1]
+var moving = 0  # switch between 0 and 1 for track animation
+var playing = true
+
+# Get sprite direction set (columns) from input vector
+func set_dir_set(vec):
+	if vec.x == 0:
+		if vec.y == -1:
+			dir_set = [0, 1]
+		if vec.y == 1:
+			dir_set = [4, 5]
+	if vec.y == 0:
+		if vec.x == -1:
+			dir_set = [2, 3]
+		if vec.x == 1:
+			dir_set = [6, 7]
+
+# Motion effect
+func _process(delta):
+	if playing:
+		moving = 0 if moving else 1
+		set_frame(dir_set[moving])
+	
+func play():
+	playing = true
+
+func stop():
+	playing = false
