063169479679c787cf2344d2cb4783d012ac97f2
Provi <zhangxian1942@live.com>
2020-08-26 21:52:04 -0700

    moved script path

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -3,15 +3,17 @@ extends Node2D
 var BrickBlock = preload("res://scenes/BrickBlock.tscn")
 
 onready var tiles = $Tiles
+onready var brickTileMap = tiles.get_node("BrickTileMap")
+onready var steelTileMap = tiles.get_node("SteelTileMap")
 
 
 func _ready():
-	# replace brick cells with BrickBlock
-	var brickTileMap = tiles.get_node("BrickTileMap")
+	_replace_bricks()
+
+
+func _replace_bricks():
 	var brick_cells = brickTileMap.get_used_cells()
 	for cell in brick_cells:
 		var brick_block = BrickBlock.instance()
-		brick_block.global_position = Vector2(cell.x, cell.y)
+		brick_block.global_position = Vector2(cell.x, cell.y) 
 	brick_cells.clear()
-	
-	print(brick_cells)
