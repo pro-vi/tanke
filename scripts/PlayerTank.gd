f428b68ad86962bdcb96af71f74bccf57cc3f251
Provi <zhangxian1942@live.com>
2020-08-26 21:53:08 -0700

    added grid snapping when tank turns

--- scripts/PlayerTank.gd
+++ scripts/PlayerTank.gd
@@ -5,21 +5,31 @@ export(int) var SPEED = 32
 onready var sprite = $Sprite
 
 var velocity = Vector2()
+var direction = "up"
+var grid = Vector2(4, 4)  # minimum grid size to snap to when turning
 
 func _physics_process(delta):
 	var input_vector = Vector2()
 	
 	if Input.is_action_pressed("ui_up"):
 		input_vector.y += -1
+		_grid_snap("up")
+		direction = "up"
 		sprite.play()
 	elif Input.is_action_pressed("ui_down"):
 		input_vector.y += 1
+		_grid_snap("down")
+		direction = "down"
 		sprite.play()
 	elif Input.is_action_pressed("ui_left"):
 		input_vector.x += -1
+		_grid_snap("left")
+		direction = "left"
 		sprite.play()
 	elif Input.is_action_pressed("ui_right"):
 		input_vector.x += 1
+		_grid_snap("right")
+		direction = "right"
 		sprite.play()
 	else:
 		sprite.stop()
@@ -29,4 +39,9 @@ func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.slide(collision.normal)
-	$Sprite.colliding = collision != null
+	sprite.colliding = collision != null
+
+
+func _grid_snap(new_dir):
+	if direction != new_dir:
+		position = position.snapped(grid)
